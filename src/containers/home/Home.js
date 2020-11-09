27a0a6809ec0e3a75d72047fbfb62e702940ae47
unknown <alexomosa001@gmail.com>
2020-11-09 15:06:38 +0300

    m

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // import { makeStyles } from '@material-ui/core/styles';
 function Home() {
-  return <div>HOME</div>;
+  return (
+    <div>
+      HOME
+      <Link to="/user">Users</Link>
+    </div>
+  );
 }
 
 export default Home;
